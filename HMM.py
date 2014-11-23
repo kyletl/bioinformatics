e6f27f144c023985dbaa292f0bc546bea9b759a5
Taylor Cathcart <tdc@Taylors-MBP.wireless.dartmouth.edu>
2014-11-23 03:51:00 -0500

    updated training

--- HMM.py
+++ HMM.py
@@ -10,6 +10,7 @@ Implementation of Hidden Markov Model.
 # imports
 import numpy as np
 import sys
+import copy
 
 # constants
 GAP = '-'
@@ -19,11 +20,12 @@ I = 'I'
 M = 'M'
 N = 'N'
 PSEUDOCOUNT = 100
+INS_DEL_SCALE = 0.2
 BEGINNING = {
 	I: {
 		I: PSEUDOCOUNT,
 		M: PSEUDOCOUNT,
-		D: PSEUDOCOUNT,
+		D: PSEUDOCOUNT * INS_DEL_SCALE,
 	},
 	B: {
 		I: PSEUDOCOUNT,
@@ -35,12 +37,12 @@ TRANSITIONS = {
 	D: {
 		D: PSEUDOCOUNT,
 		M: PSEUDOCOUNT,
-		I: PSEUDOCOUNT,
+		I: PSEUDOCOUNT * INS_DEL_SCALE,
 	},
 	I: {
 		I: PSEUDOCOUNT,
 		M: PSEUDOCOUNT,
-		D: PSEUDOCOUNT,
+		D: PSEUDOCOUNT * INS_DEL_SCALE,
 	},
 	M: {
 		I: PSEUDOCOUNT,
@@ -64,6 +66,7 @@ END = {
 }
 VOCAB = set("ARNDCQEGHILKMFPSTWYV")
 STATES = set([B, I, D, M, N])
+BODY_STATES = set([I, D, M])
 
 
 """A class to define a profile-Hidden Markov Model.
@@ -75,11 +78,105 @@ Input:
 	start (optional) - start state, must be in states
 """
 
-# class ProfileHMM:
+class ProfileHMM:
 
-# 	def __init__(self, length, vocab):
-# 		self.length = length
-# 		self.vocab = vocab
+	def __init__(self, hmm):
+		# self.length = length
+		self.vocab = VOCAB
+
+		# a[t][i][j] 
+		# i.e. chance of transition from state i at node t to state j
+		self.a = copy.deepcopy(hmm.a)
+
+		# b[t][i][e] = chances of emission e in state i at node t
+		self.b = copy.deepcopy(hmm.b)
+
+	def source_states(self, pos, state):
+		"""Return the set of all source states, in the form of coordinates, 
+		to the specified state.
+		"""
+		assert state in STATES
+		assert pos <= self.length or (pos == self.length + 1 and state is N)
+		sources = set()
+
+		if (state is not N or pos == self.length + 1) and state is not B:
+			if pos == 0:
+				if state is I:
+					bgn = (0, B)
+					sources.add(bgn)
+					return sources
+				else:
+					return sources
+
+			elif pos > self.length + 1:
+				return sources
+
+			elif pos < 0:
+				return sources
+			
+			elif state is not I:
+				src_pos = pos - 1
+
+			else:
+				src_pos = pos
+			
+			d_src = (src_pos, D)
+			m_src = (src_pos, M)
+			i_src = (src_pos, I)
+			sources.add(d_src)
+			sources.add(m_src)
+			sources.add(i_src)
+			
+			return sources
+
+		return sources
+
+	def dest_states(self, pos, state):
+		"""Inverse of source_states function."""
+		assert state in STATES
+		assert pos <= self.length
+		sources = set()
+
+		if (state is not B or pos == 0) and state is not N:
+			if pos == self.length:
+					end = (self.length + 1, N)
+					sources.add(end)
+					return sources
+
+			elif pos > self.length:
+				return sources
+
+			elif pos < 0:
+				return sources
+
+			dest_pos = pos + 1
+			d_dest = (dest_pos, D)
+			m_dest = (dest_pos, M)
+			i_dest = (pos, I)
+			sources.add(d_dest)
+			sources.add(m_dest)
+			sources.add(i_dest)
+
+			return sources
+
+		return sources
+
+	def check_forward(self, sequence):
+		touched = set()
+		alpha = [{}]
+
+		# init data structure for all states
+		alpha[0][(0, B)] = 1
+		alpha[0][(0, I)] = 0
+		for state in BODY_STATES:
+			for k in range(1, self.length + 1)
+				alpha[0][(k, state)] = 0
+		alpha[0][(self.length + 1, N)] = 0
+
+		# record all possible states, represented as tuples (node_number, state_type)
+		all_states = set(alpha[0].values())
+
+		for t in range(1, len(sequence)+1)
 
 # 		self.vocab_size = len(vocab)
 
@@ -118,66 +215,69 @@ Input:
 # 	def converge(self):
 # 		pass
 
-# 	"""Function to scan forward through a sequence and find probabilities."""
-# 	def scan_forward(self, sequence):
-# 		width = len(sequence)
-# 		result = {state: [0] * width for state in self.states}
+	"""Function to scan forward through a sequence and find probabilities."""
+	def eval_forward(self, sequence):
+		pass
 
-# 		# init time 0
-# 		for state in self.states:
-# 			if state in self.emits:
-# 				result[state][0] = self.pi[state] * self.b[state][sequence[0]]
-# 			else:
-# 				result[state][0] = self.pi[state]
+	# 	width = len(sequence)
+	# 	result = {state: [0] * width for state in self.states}
 
-# 		# induction from base case
-# 		for t in range(1, width):
-# 			# calculate prob of each destination state
-# 			for dest in self.states:
-# 				# for every source state in previous time
-# 				for source in self.states:
-# 					# add prob of transitioning into dest state
-# 					result[dest][t] += result[source][t-1] * self.a[source][dest]
-# 				# scale by emission probability if needed
-# 				if dest in self.emits:
-# 					result[dest][t] *= self.b[dest][sequence[t]]
+	# 	# init time 0
+	# 	for state in self.states:
+	# 		if state in self.emits:
+	# 			result[state][0] = self.pi[state] * self.b[state][sequence[0]]
+	# 		else:
+	# 			result[state][0] = self.pi[state]
 
-# 		# result holds prob of each state/time given prefix
-# 		return result
+	# 	# induction from base case
+	# 	for t in range(1, width):
+	# 		# calculate prob of each destination state
+	# 		for dest in self.states:
+	# 			# for every source state in previous time
+	# 			for source in self.states:
+	# 				# add prob of transitioning into dest state
+	# 				result[dest][t] += result[source][t-1] * self.a[source][dest]
+	# 			# scale by emission probability if needed
+	# 			if dest in self.emits:
+	# 				result[dest][t] *= self.b[dest][sequence[t]]
 
-# 	"""Function to scan backward through a sequence and find probabilities."""
-# 	def scan_backward(self, sequence):
-# 		width = len(sequence)
-# 		result = {state: [0] * width for state in self.states}
+	# 	# result holds prob of each state/time given prefix
+	# 	return result
 
-# 		# init final time
-# 		for state in self.states[-1]:
-# 			result[state][width-1] = 1
+	"""Function to scan backward through a sequence and find probabilities."""
+	def eval_backward(self, sequence):
+		pass
+		# width = len(sequence)
+		# result = {state: [0] * width for state in self.states}
 
-# 		result[I][width-1] = 1
+		# # init final time
+		# for state in self.states[-1]:
+		# 	result[state][width-1] = 1
 
-# 		# induction from base case
-# 		# for t in range(width-2, -1, -1):
-# 			# check delete state
+		# result[I][width-1] = 1
 
-# 			# check match state
+		# # induction from base case
+		# for t in range(width-2, -1, -1):
+		# 	# check delete state
 
-# 			# check insert state
+		# 	# check match state
 
-# 			# calculate prob of each source state
-# 			# for source in self.states:
-# 			# 	# for every destination state
-# 			# 	for dest in self.states:
-# 			# 		# add prob of transitioning from source state
-# 			# 		prob = result[dest][t+1] * self.a[source][dest]
-# 			# 		if dest in self.emits:
-# 			# 			prob *= self.b[dest][sequence[t+1]]
-# 			# 		result[source][t] += prob
+		# 	# check insert state
 
-# 		# result holds prob of each state/time given suffix
-# 		return result
+		# 	# calculate prob of each source state
+		# 	for source in self.states:
+		# 		# for every destination state
+		# 		for dest in self.states:
+		# 			# add prob of transitioning from source state
+		# 			prob = result[dest][t+1] * self.a[source][dest]
+		# 			if dest in self.emits:
+		# 				prob *= self.b[dest][sequence[t+1]]
+		# 			result[source][t] += prob
 
-class ProfileHMMHelper:
+		# # result holds prob of each state/time given suffix
+		# return result
+
+class AlignedProfileHMMInit:
 
 	def __init__(self, profiles):
 		vocab = VOCAB
@@ -223,13 +323,13 @@ class ProfileHMMHelper:
 		while col < full_width:
 			# for insert state, add count to current node insert state
 			if col_status[col] is I:
-				ProfileHMMHelper._add_count(b[node][I], col_count[col])
+				AlignedProfileHMMInit._add_count(b[node][I], col_count[col])
 				col += 1
 			# otherwise, create a new node and add to its match state
 			else:
 				b.append({I: dict(empty_count), M: dict(empty_count)})
 				node += 1
-				ProfileHMMHelper._add_count(b[node][M], col_count[col])
+				AlignedProfileHMMInit._add_count(b[node][M], col_count[col])
 				col += 1
 
 		model_width = len(b)
@@ -267,7 +367,6 @@ class ProfileHMMHelper:
 				col += 1
 
 		### NORMALIZATION ###
-		assert len(a) == len(b)
 
 		for node in range(model_width):
 			# normalize transition matrix
@@ -286,7 +385,7 @@ class ProfileHMMHelper:
 
 		self.a = a
 		self.b = b
-
+		# self.hmm = ProfileHMM()
 
 	@staticmethod
 	def _add_count(ledger, addition):
